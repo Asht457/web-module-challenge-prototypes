241a37780d093f822a88f264a66e5e460dd553e1
Liam Thomson <ash.t45@outlook.com>
2022-09-11 12:52:04 -0500

    finished Person constructor with prototypes

--- index.js
+++ index.js
@@ -15,8 +15,24 @@
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
+function Person(name, age, stomach = []) {
+  this.name = name;
+  this.age = age;
+  this.stomach = stomach;  
+}
 
+Person.prototype.eat = function(food) {
+if(this.stomach.length < 10) {
+  this.stomach.push(food);
+}
+}
+
+Person.prototype.poop = function()  {
+this.stomach = []
+}
+
+Person.prototype.toString = function() {
+  return `${this.name}, ${this.age}`
 }
 
 
